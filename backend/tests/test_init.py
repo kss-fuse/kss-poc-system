5da6e621a40093193192343a3d69591ab2e361c5
kss-fuse <h_fuse@systemsolution.co.jp>
2026-03-06 17:38:46 +0900

    CIの仕組みを構築

---
+++ backend/tests/test_init.py
@@ -0,0 +1,10 @@
+from src.functions.init.app import lambda_handler
+
+
+def test_lambda_handler():
+    event = {}
+    context = {}
+
+    result = lambda_handler(event, context)
+
+    assert result["statusCode"] == 200
